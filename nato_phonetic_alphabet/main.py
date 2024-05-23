b017441eb26a4a353afa3140b7b53a7a87736595
yenthybui <alys.tbui@gmail.com>
2024-05-23 11:27:14 -0400

    update phonetic conversion codes

--- nato_phonetic_alphabet/main.py
+++ nato_phonetic_alphabet/main.py
@@ -3,7 +3,15 @@ df = pd.read_csv('nato_phonetic_alphabet.csv')
 
 nato = {row.letter:row.code for (index,row) in df.iterrows()}
 
-word = input('Please input a word you want to convert to phonetic code: ').upper()
+def generate_phonetic():
+    word = input('Please input a word you want to convert to phonetic code: ').upper()
+    
+    try:
+        code = [nato[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(code)
 
-code = [nato[letter] for letter in word]
-print(code)
+generate_phonetic()
